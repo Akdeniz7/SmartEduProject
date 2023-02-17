aea1af0358f8de47a5503ae9834670a89c21fa04
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-02-17 15:42:09 +0300

    index and about page added

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const express = require('express');
 
 const app = express();
 
+//TEMPLATE ENGINE
+app.set('view engine', 'ejs');
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+//ROUTES
 app.get('/', (req, res) => {
-  res.send('INDEX');
+  res.status(200).render('index', {
+    page_name: "index"
+  });
 });
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about', {
+    page_name: "about"
+  });
+});
+
 
 const port = 3000;
 app.listen(port, () => {
