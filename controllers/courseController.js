8c2dfd01efa249817e306b2fc833dbbec8a9f1ea
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-02-24 18:22:54 +0300

    search bar and contact page added

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -23,6 +23,7 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const categorySlug = req.query.categories;
+    const query = req.query.search;
 
     const category = await Category.findOne({ slug: categorySlug });
 
@@ -32,7 +33,20 @@ exports.getAllCourses = async (req, res) => {
       filter = { category: category._id };
     }
 
-    const courses = await Course.find(filter).sort('-createdAt');
+    if (query) {
+      filter = { name: query };
+    }
+
+    if (!query && !categorySlug) {
+      (filter.name = ''), (filter.category = null);
+    }
+
+    const courses = await Course.find({
+      $or: [
+        { name: { $regex: '.*' + filter.name + '.*', $options: 'i' } },
+        { category: filter.category },
+      ],
+    }).sort('-createdAt').populate('user');
     const categories = await Category.find();
 
     res.status(200).render('courses', {
@@ -50,15 +64,17 @@ exports.getAllCourses = async (req, res) => {
 
 exports.getCourse = async (req, res) => {
   try {
-    const user = await User.findById(req.session.userID)
+    const user = await User.findById(req.session.userID);
     const course = await Course.findOne({ slug: req.params.slug }).populate(
       'user'
     );
+    const categories = await Category.find();
 
     res.status(200).render('course', {
       course,
       page_name: 'courses',
-      user
+      user,
+      categories
     });
   } catch (error) {
     res.status(400).json({
@@ -70,9 +86,8 @@ exports.getCourse = async (req, res) => {
 
 exports.enrollCourse = async (req, res) => {
   try {
-    
-    const user = await User.findById(req.session.userID)
-    await user.courses.push({_id: req.body.course_id})
+    const user = await User.findById(req.session.userID);
+    await user.courses.push({ _id: req.body.course_id });
     await user.save();
 
     res.status(200).redirect('/users/dashboard');
@@ -86,9 +101,8 @@ exports.enrollCourse = async (req, res) => {
 
 exports.releaseCourse = async (req, res) => {
   try {
-    
-    const user = await User.findById(req.session.userID)
-    await user.courses.pull({_id: req.body.course_id})
+    const user = await User.findById(req.session.userID);
+    await user.courses.pull({ _id: req.body.course_id });
     await user.save();
 
     res.status(200).redirect('/users/dashboard');
@@ -99,4 +113,3 @@ exports.releaseCourse = async (req, res) => {
     });
   }
 };
-
