2681b933e32f57a5f9c1ad80453e8bcff7291b08
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-02-25 15:47:13 +0300

    flash messages added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
 const categoryRoute = require('./routes/categoryRoute');
@@ -38,12 +39,17 @@ app.use(
     store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1/smartedu-db' }),
   })
 );
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
 
 //Routes
-app.use('*', (req,res,next) => {
+app.use('*', (req, res, next) => {
   userIN = req.session.userID;
   next();
-})
+});
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
 app.use('/categories', categoryRoute);
--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer');
 
 exports.getIndexPage = (req, res) => {
-  console.log(req.session.userID)
+  console.log(req.session.userID);
   res.status(200).render('index', {
     page_name: 'index',
   });
@@ -32,7 +32,8 @@ exports.getContactPage = (req, res) => {
 };
 
 exports.sendEmail = async (req, res) => {
-  const outputMessage = `
+  try {
+    const outputMessage = `
     <h1> Mail Details </h1>
     <ul> 
       <li> Name: ${req.body.name}</li>
@@ -41,34 +42,37 @@ exports.sendEmail = async (req, res) => {
     <h1> Message </h1>
 
     <p>${req.body.message} </p>
-  ` 
-  let transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true, // true for 465, false for other ports
-    auth: {
-      user: "example@gmail.com", // gmail hesabı
-      pass: "#", // gmail şifresi veya uygulama şifresi
-    },
-  });
+  `;
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+        user: 'example@gmail.com', // gmail hesabı
+        pass: '#', // gmail şifresi veya uygulama şifresi
+      },
+    });
 
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"Smart EDU Contact Form" <example@gmail.com>', // sender address
-    to: "gidecekmail@gmail.com", // list of receivers
-    subject: "Smart EDU Contact Form New Message ✔", // Subject line
-    html: outputMessage, // html body
-  });
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: '"Smart EDU Contact Form" <example@gmail.com>', // sender address
+      to: 'gidecekmail@gmail.com', // list of receivers
+      subject: 'Smart EDU Contact Form New Message ✔', // Subject line
+      html: outputMessage, // html body
+    });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+    console.log('Message sent: %s', info.messageId);
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
 
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    // Preview only available when sending through an Ethereal account
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
-  res.status(200).redirect('contact');
+    req.flash('success', 'We Received your message succesfully');
 
+    res.status(200).redirect('contact');
+  } catch (err) {
+    req.flash('error', `Something goes wrong! ${err}`);
+    res.status(200).redirect('contact');
+  }
 };
-
-
