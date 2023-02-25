36c2f84f672f45ecf7dc69d52cef9735eedf560f
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-02-25 17:26:18 +0300

    course deletion added

--- models/User.js
+++ models/User.js
@@ -29,10 +29,15 @@ const UserSchema = new Schema({
 
 UserSchema.pre('save', function (next) {
   const user = this;
-  bcrypt.hash(user.password, 10, (error, hash) => {
-    user.password = hash;
-    next();
-  });
+  if (!user.isModified('password')) return next();
+  
+  bcrypt.genSalt(10,function(err, salt) {
+    if(err) return next(err);
+    bcrypt.hash(user.password, salt, function(error, hash) {
+      user.password = hash;
+      next();
+    });
+  })
 });
 
 const User = mongoose.model('User', UserSchema);
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const courseController = require('../controllers/courseController');
 const roleMiddleware = require('../middlewares/roleMiddleware')
-
 const router = express.Router();
-
 router.route('/').post(roleMiddleware(["teacher", "admin"]), courseController.createCourse); //http://localhost:3000/courses
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
+router.route('/:slug').delete(courseController.deleteCourse);
 router.route('/enroll').post(courseController.enrollCourse);
 router.route('/release').post(courseController.releaseCourse);
 
