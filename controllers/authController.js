4e8c26eac2459ca4f64557c9437697940106ef06
Ahmet Sezer <ahmetsezerakdeniz19@gmail.com>
2023-02-25 16:26:00 +0300

    validation added.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const { validationResult } = require('express-validator');
 const User = require('../models/User');
 const Category = require('../models/Category');
 const Course = require('../models/Course');
@@ -9,10 +10,15 @@ exports.createUser = async (req, res) => {
 
     res.status(201).redirect('/login');
   } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      error,
-    });
+    const errors = validationResult(req);
+    console.log(errors);
+    console.log(errors.array()[0].msg);
+
+    for (let i = 0; i <errors.array().length; i++) {
+      req.flash("error", `${errors.array()[i].msg}`);
+    }
+
+    res.status(400).redirect('/register');
   }
 };
 
@@ -20,13 +26,21 @@ exports.loginUser = async (req, res) => {
   try {
     const { email, password } = req.body;
     let user = await User.findOne({ email });
-    let same = await bcrypt.compare(password, user.password);
-    if (same) {
-      // USER SESSION
-      req.session.userID = user._id;
-      res.status(200).redirect('/users/dashboard');
+
+    if (user) {
+      let same = await bcrypt.compare(password, user.password);
+
+      if (same) {
+        // USER SESSION
+        req.session.userID = user._id;
+        res.status(200).redirect('/users/dashboard');
+      } else {
+        req.flash("error", "Your password is not correct!");
+        res.status(400).redirect('/login');
+      }
     } else {
-      res.send(err);
+      req.flash("error", "User is not exist!");
+      res.status(400).redirect('/login');
     }
   } catch (error) {
     res.status(400).json({
